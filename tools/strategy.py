8a4f64fbdf97dba9ba6b1ab8e9522a3512a31e00
Bruno C. Santos <devbrunocsantos@gmail.com>
2026-02-12 23:24:57 -0300

    projeção

--- tools/strategy.py
+++ tools/strategy.py
@@ -305,6 +305,8 @@ class CashAndCarryBot:
             # Projeção do Funding Real
             projected_return = (funding_rate * funding_frequency_daily) * PAYBACK_PERIOD_DAYS
 
+            LOGGER.info(f"Projeção de Funding: {symbol} | {projected_return}")
+
             if projected_return < hurdle_rate:
                 return False, funding_rate, "LOW_PROFIT_VS_FEES"
 
